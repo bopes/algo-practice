251011669194498e212c5799de9c604efb8ddaad
bopes <clcblee@gmail.com>
2016-06-01 21:15:19 -0700

    Complete cyclic rotation

--- arrays/cyclic-rotation.rb
+++ arrays/cyclic-rotation.rb
@@ -1,4 +1,16 @@
 # Given an array, move all elements after that index to the beginning of the array a certain number of times
 
+def solution(a, k)
+
+  if a != []
+    k.times do
+      a = [a[-1]] + a[0..-2]
+    end
+  end
+
+  a
+
+end
+
 
 
