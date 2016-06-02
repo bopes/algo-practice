ba013be0728cd4365e6b664d1daaed5e4ce8b224
bopes <clcblee@gmail.com>
2016-06-01 21:44:33 -0700

    Complete frog jumps

--- time-complexity/frog-jump.rb
+++ time-complexity/frog-jump.rb
@@ -1,4 +1,14 @@
 # Count minium jumps to cover a certain distance
 
+def solution(x, y, d)
 
+  distance = y - x
+
+  if distance % d == 0
+    distance / d
+  else
+    distance / d + 1
+  end
+
+end
 
