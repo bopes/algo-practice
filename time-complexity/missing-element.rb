eba81358cdd44f54a182a77ba9932fc584009827
bopes <clcblee@gmail.com>
2016-06-01 22:14:30 -0700

    Complete missing element

--- time-complexity/missing-element.rb
+++ time-complexity/missing-element.rb
@@ -1,2 +1,16 @@
 # Given an array for integers from 1 to N with one integer missing, find the missing integer.
 
+def solution(a)
+
+  a.sort!
+
+  i = 0
+  while i < a.length do
+    break if a[i] == i + 2
+    i += 1
+  end
+
+  i + 1
+
+end
+
