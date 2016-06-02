569486b8b19764d7cbae262464d15029085daac3
bopes <clcblee@gmail.com>
2016-06-01 21:37:26 -0700

    Complete odd occurrences

--- arrays/odd-occurrences.rb
+++ arrays/odd-occurrences.rb
@@ -1,1 +1,14 @@
 # Given an array with odd num of elements, all duplicates except one, find the sole non-duplicated elements
+
+def solution(a)
+
+  counts = Hash.new(0)
+
+  a.each do |num|
+    counts[num] += 1
+    counts.delete(num) if counts[num] == 2
+  end
+
+  counts.key(1)
+
+end
