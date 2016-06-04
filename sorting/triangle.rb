6bb925f273575f9796642389795c13c89fb3f0f2
bopes <clcblee@gmail.com>
2016-06-04 16:21:04 -0700

    Complete triangle

--- sorting/triangle.rb
+++ sorting/triangle.rb
@@ -1,2 +1,19 @@
 # Find if an array contains a triplet that can form a triangle. Time complexity is O(N*logN).
 
+def solution(a)
+
+  s = a.sort
+
+  i, ans = 2, 0
+
+  while i < s.length do
+    if s[i] < s[i-1] + s[i-2] && s[i-1] < s[i] + s[i-2] && s[i-2] < s[i-1] + s[i]
+      ans = 1
+      break
+    end
+    i += 1
+  end
+
+  ans
+
+end
