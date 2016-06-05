9df85f3dcc5d037d5353cff047895406e6f9f303
bopes <clcblee@gmail.com>
2016-06-04 17:20:51 -0700

    Refactor solution for brevity

--- sorting/max_product_of_three.rb
+++ sorting/max_product_of_three.rb
@@ -3,20 +3,10 @@
 def solution(a)
 
   s = a.sort
-  s_neg = s.select { |int| int < 0 }
-
 
   norm = s[-3] * s[-2] * s[-1]
+  neg  = s[0]  * s[1]  * s[-1]
 
-  some_neg = s[0] * s[1] * s[-1]
-
-  if s_neg.length > 3
-    all_neg = s_neg[-3] * s_neg[-2] * s_neg[-1]
-  else
-    all_neg = norm
-  end
-
-  [norm, some_neg, all_neg].max
+  [norm, neg].max
 
 end
-
