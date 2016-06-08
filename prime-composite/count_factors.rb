63592c4064a052548ff5c17c2ce843e0364c0b96
bopes <clcblee@gmail.com>
2016-06-08 09:40:58 -0700

    Complete count factors

---
+++ prime-composite/count_factors.rb
@@ -0,0 +1,18 @@
+# Find the count of factors for a given positive integer
+
+def solution(n)
+
+  factors_count = 0
+
+  i = 1
+  while i * i < n
+    factors_count += 2 if n % i == 0
+    i += 1
+  end
+
+  factors_count += 1 if i * i == n
+
+  factors_count
+
+end
+
