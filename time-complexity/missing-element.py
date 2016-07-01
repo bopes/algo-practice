03f55b1572cb5567e8c977c21257094a096dcbc6
bopes <clcblee@gmail.com>
2016-07-01 10:41:16 -0700

    Complete missing element py

--- time-complexity/missing-element.py
+++ time-complexity/missing-element.py
@@ -1,2 +1,16 @@
 # Given an array for integers from 1 to N with one integer missing, find the missing integer.
 
+def solution(A):
+
+    n = range(1,len(A)+2)
+    availableValues = {}
+
+    for i in n:
+        availableValues[i] = True
+
+    for i in A:
+        availableValues.pop(i, None)
+
+    for key in availableValues:
+        return key
+
